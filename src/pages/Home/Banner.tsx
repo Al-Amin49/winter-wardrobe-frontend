9389d824a2af0e2cf96d6015689b0081bc03e40c
Al-Amin49 <alamin496832@gmail.com>
2024-07-03 01:13:44 +0600

    hero section fixed

--- src/pages/Home/Banner.tsx
+++ src/pages/Home/Banner.tsx
@@ -6,52 +6,54 @@ import "swiper/css/navigation";
 import { useRef } from "react";
 import "../../styles/Banner.css";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
-import banner1 from "../../assets/img/banner/1.jpg";
-import banner2 from "../../assets/img/banner/2.png";
-import banner3 from "../../assets/img/banner/3.jpg";
+import banner1 from "../../assets/img/banner/1-removebg-preview.png";
+import banner2 from "../../assets/img/banner/2-removebg-preview.png";
+import banner3 from "../../assets/img/banner/21.png";
 import { Link } from "react-router-dom";
+import Container from "../../components/Container";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const Banner = () => {
   const progressCircle = useRef<SVGSVGElement>(null);
   const progressContent = useRef<HTMLSpanElement>(null);
-  // const onAutoplayTimeLeft = (_s:unknown, time:number, progress:number) => {
-  //   if (progressCircle.current) {
-  //     // Null check before accessing properties
-  //     progressCircle.current.style.setProperty("--progress", String(1 - progress));
-  //   }
-  //   if (progressContent.current) {
-  //     // Null check before accessing properties
-  //     progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
-  //   }
-  // };
 
-  const bannerData=[
+  const [text] = useTypewriter({
+    words: [
+      "a full winter outfit",
+      "warm meals for a month",
+      "shelter for a night",
+    ],
+    loop: 0,
+    delaySpeed: 2000,
+  });
+
+  const bannerData = [
     {
       image: banner1,
-      title: "Your $50 Donation Provides a Full Winter Outfit",
-      description: "Let's come together to spread the warmth of compassion and generosity. Your simple act of donating gently used winter clothing can ignite a spark of hope in someone's life.",
+      title: "Your $50 Donation Provides ",
+      description: "Let's come together to spread the warmth of compassion and generosity.",
 
     },
     {
       image: banner2,
-      title: "Your $50 Donation Provides a Full Winter Outfit",
-      description: "Let's come together to spread the warmth of compassion and generosity. Your simple act of donating gently used winter clothing can ignite a spark of hope in someone's life.",
+      title: "Your $50 Donation Provides ",
+      description: "Let's come together to spread the warmth of compassion and generosity.",
 
     },
     {
       image: banner3,
-      title: "Your $50 Donation Provides a Full Winter Outfit",
-      description: "Let's come together to spread the warmth of compassion and generosity. Your simple act of donating gently used winter clothing can ignite a spark of hope in someone's life.",
+      title: "Your $50 Donation Provides ",
+      description: "Let's come together to spread the warmth of compassion and generosity.",
 
     },
   ]
   return (
-    <div>
+    <Container>
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 4500,
           disableOnInteraction: false,
         }}
         pagination={{
@@ -59,35 +61,42 @@ const Banner = () => {
         }}
         navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
-        // onAutoplayTimeLeft={onAutoplayTimeLeft}
-        className="mySwiper"
+        className="mySwiper "
       >
-       {
-        bannerData.map(data=>(
-          <SwiperSlide>
-          <img src={data.image} alt="" 
-          loading="lazy"
+        {
+          bannerData.map(data => (
+            <SwiperSlide >
 
-          className=" w-[500px] h-[500px]"
-          />
-          <div className="absolute bottom-0 lg:bottom-auto ">
-            <div>
-            <h3 className="text-xl md:text-4xl lg:text-5xl  text-center font-extrabold text-white">
-            {data.title}
-            </h3>
-            <p className="text-base md:text-lg lg:text-xl w-[50%] text-center  font-bold text-white py-3 lg:py-8">
-              {data.description}
-            </p>
-            <Link to="/all-clothe">
-            <button className="mb-4 lg:mb-0 btn btn-primary font-bold text-white">Donate Now</button>
-            </Link>
-            </div>
-          </div>
-        </SwiperSlide>
-        ))
-       }
-        
-        
+              <div className="mt-24 flex flex-col lg:flex-row justify-evenly  items-center lg:h-[450px] ">
+                <div className="">
+                  <h3 className="text-xl md:text-3xl w-[90%] mx-auto   font-extrabold text-primary">
+                    {data.title}
+                    <span className="text-secondary">
+                      {text}
+                      <Cursor cursorStyle="|" />
+                    </span>
+                  </h3>
+                  <p className="text-base md:text-lg lg:text-xl w-[80%] mx-auto   font-bold  py-3 lg:py-8">
+                    {data.description}
+                  </p>
+                  <Link to="/all-clothe">
+                    <button className="mb-4 lg:mb-0 btn btn-primary font-bold text-white">Donate Now</button>
+                  </Link>
+                </div>
+
+                <div>
+                  <img src={data.image} alt=""
+                    loading="lazy"
+
+                    className="w-[500px] h-[500px]"
+                  />
+                </div>
+              </div>
+            </SwiperSlide>
+          ))
+        }
+
+
 
         <div className="autoplay-progress" slot="container-end">
           <svg viewBox="0 0 48 48" ref={progressCircle}>
@@ -96,7 +105,7 @@ const Banner = () => {
           <span ref={progressContent}></span>
         </div>
       </Swiper>
-    </div>
+    </Container>
   );
 };
 
