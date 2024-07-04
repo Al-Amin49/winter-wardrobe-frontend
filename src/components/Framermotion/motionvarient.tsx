a6d288c3346bfcb7933ff39081e40e5207a4ea67
Al-Amin49 <alamin496832@gmail.com>
2024-07-04 11:41:45 +0600

    modified motionContainer

--- src/components/Framermotion/motionvarient.tsx
+++ src/components/Framermotion/motionvarient.tsx
@@ -1,11 +1,11 @@
 export const motionContainer = {
-    initial: { y: 100, opacity: 0 },
+    initial: { y: 70, opacity: 0 },
     whileInView: { y: 0, opacity: 1 },
     transition: {
-      delay: 1,
-      y: { type: "tween", stiffness: 60 },
+      delay: 0.7,
+      y: { type: "spring", stiffness: 60,  },
       opacity: { duration: 0.5 },
-      ease: "circInOut",
+      ease: "easeIn",
       duration: 1,
     },
   };
