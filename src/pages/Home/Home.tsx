098de006550826a26a55011055824bffbeb82376
Al-Amin49 <alamin496832@gmail.com>
2024-08-02 00:06:05 +0600

    add video section

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,23 @@
 import Banner from "./Banner";
-import Testimonials from './Testimonials';
-import Gallery from './Gallery';
-import Article from './Article';
-import AboutUs from './AboutUs';
+import Testimonials from "./Testimonials";
+import Video from "./Video";
+import Article from "./Article";
+import AboutUs from "./AboutUs";
 import RecentEvents from "./RecentEvents";
 import ClothePost from "./ClothePost";
 
-
 const Home = () => {
-    return (
-        <div>
-            <Banner/>
-            <ClothePost/>
-            <Article/>
-            <RecentEvents/>
-            <Gallery/>
-            <AboutUs/>
-            <Testimonials/>
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+      <ClothePost />
+      <Article />
+      <RecentEvents />
+      <Video />
+      <AboutUs />
+      <Testimonials />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/pages/Home/Video.tsx
@@ -0,0 +1,46 @@
+import Container from '../../components/Container';
+import donateCover from '../../assets/img/donateCover.png';
+import donateVideo from '../../assets/img/donation.mp4'
+import { Link } from 'react-router-dom';
+import Subline from '../../components/Subline';
+const Video = () => {
+  return( 
+  <div>
+    <Container >
+        <div className="container mx-aut text-center">
+        <h3 className="text-3xl font-bold text-primary text-center pt-20">
+          Donatin Drive
+        </h3>
+        <div className='pb-5'>
+          <Subline bgPrimary={false} />
+        </div>
+            <div className="relative flex items-center justify-center">
+                <video className="w-full rounded-3xl"
+                autoPlay muted loop playsInline poster={donateCover}
+                >
+                    <source 
+                    src={donateVideo}
+                    type='video/mp4'
+                    ></source>
+                </video>
+                <div className="absolute h-full w-full rounded-3xl bg-black/40"></div>
+               <div className='absolute'>
+               <p className=" text-xl text-secondary mb-2 font-bold tracking-tighter lg:text-3xl">
+                Join Our Community Charity Donation Center
+                </p>
+                <Link to="/all-clothe">
+                <button
+                className="btn btn-primary text-white"
+              >
+                Donate Now
+              </button>
+                </Link>
+               </div>
+        </div>
+        </div>
+    </Container>
+
+  </div>)
+};
+
+export default Video;
