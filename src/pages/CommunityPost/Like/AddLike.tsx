3ec04477437355332c4acee6f50a170de6850f04
Al-Amin49 <alamin496832@gmail.com>
2024-09-22 15:56:18 +0600

    add like succesfully

--- src/pages/CommunityPost/Like/AddLike.tsx
+++ src/pages/CommunityPost/Like/AddLike.tsx
@@ -1,24 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Heart } from "lucide-react";
-import { TCommunityPost, TCommunityUser } from "@/types"; // Assuming this is the correct path for types
+import { TCommunityPost, TCommunityUser } from "@/types"; 
 import { useToggleLikeMutation } from "../../../redux/api/likeApi";
 import { useState } from "react";
 import { useAppSelector } from "../../../redux/hook";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 interface AddLikeProps {
   post: TCommunityPost;
 }
 
 const AddLike = ({ post }: AddLikeProps) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const userInfo: any = useAppSelector((state) => state.auth.userInfo);
+  const userInfo:any = useAppSelector((state) => state.auth.userInfo);
   const singleUser = userInfo?.data?.user;
+
   const [toggleLike, { isLoading }] = useToggleLikeMutation();
-  const [localLikes, setLocalLikes] = useState(post.likes); // Local state for like count
+  const [localLikes, setLocalLikes] = useState(post.likes); 
+
+  // Check if the logged-in user has already liked the post
   const [liked, setLiked] = useState(
-    post?.likedBy?.some((user: TCommunityUser) => user._id === singleUser._id)
-  ); // Check if user has already liked the post
+    post?.likedBy?.some((user: TCommunityUser) => user?._id === singleUser?._id)
+  );
+
+  const navigate = useNavigate();
 
   const handleLike = async () => {
+    if (!singleUser) {
+      toast.info("Login to like the post");
+      navigate("/login");
+      return;
+    }
+
     try {
       // Optimistically toggle like/unlike
       if (liked) {
@@ -26,7 +39,7 @@ const AddLike = ({ post }: AddLikeProps) => {
       } else {
         setLocalLikes(localLikes + 1);
       }
-      setLiked(!liked); // Toggle the liked state
+      setLiked(!liked); 
 
       // Call the mutation to update on server
       await toggleLike(post._id).unwrap();
@@ -48,7 +61,7 @@ const AddLike = ({ post }: AddLikeProps) => {
       disabled={isLoading}
       className={`flex items-center font-semibold transition-colors duration-200 ${
         liked ? "text-green-500" : "text-gray-600 hover:text-red-500"
-      }`}
+      }`} 
     >
       <Heart className="mr-2" />
       <span>{localLikes}</span>
